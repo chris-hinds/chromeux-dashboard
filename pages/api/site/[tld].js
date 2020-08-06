d084b83e6c23b20401a2ec6c3434d2c8c233cec8
Chris <chris.hinds@bbc.co.uk>
2020-08-06 21:35:52 +0100

    console log request body

--- pages/api/site/[tld].js
+++ pages/api/site/[tld].js
@@ -1,40 +1,50 @@
-import axios from "axios"
+import axios from "axios";
 
-const SITE_BASE_URL = "https://www.bbc.com"
-const CONNECTION_TYPE = "3G"
-const DEVICE = "PHONE"
+const SITE_BASE_URL = "https://www.bbc.com";
+const CONNECTION_TYPE = "3G";
+const DEVICE = "PHONE";
 const METRICS = [
-  "first_contentful_paint", "first_input_delay", "largest_contentful_paint", "cumulative_layout_shift"
-]
+  "first_contentful_paint",
+  "first_input_delay",
+  "largest_contentful_paint",
+  "cumulative_layout_shift",
+];
 
 const REQUEST_CONFIG = {
   params: {
-    key: process.env.CRUX_API_KEY
-  }
-}
+    key: process.env.CRUX_API_KEY,
+  },
+};
 
 export default async (req, res) => {
-  const { query: {tld}} = req
+  const {
+    query: { tld },
+  } = req;
 
   const REQUEST_BODY = {
-    "effectiveConnectionType": CONNECTION_TYPE,
-    "formFactor": DEVICE,
-    "metrics": METRICS,
-    "url": `${SITE_BASE_URL}/${tld}`
-  }
+    effectiveConnectionType: CONNECTION_TYPE,
+    formFactor: DEVICE,
+    metrics: METRICS,
+    url: `${SITE_BASE_URL}/${tld}`,
+  };
+
+  console.log(`Request Body --------` + JSON.stringify(REQUEST_BODY));
 
   try {
-    const cruxResponse = await axios.post(process.env.CRUX_API_BASE_ENDPOINT, 
-    REQUEST_BODY, REQUEST_CONFIG)
+    const cruxResponse = await axios.post(
+      process.env.CRUX_API_BASE_ENDPOINT,
+      REQUEST_BODY,
+      REQUEST_CONFIG
+    );
 
-    const {data, status} = cruxResponse
+    const { data, status } = cruxResponse;
 
     if (status != 200) {
-      res.send('error fetch data')
+      res.send("error fetch data");
     }
 
-    res.send(data)
-  } catch(error) {
-    res.send(error)
+    res.send(data);
+  } catch (error) {
+    res.send(error);
   }
-}
+};
